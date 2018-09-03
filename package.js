9efc666165aaf2259fdc7bba89932373b77a8153
Mike Cunneen <mike@appworkshop.net>
2018-09-03 13:47:30 +0800

    Update underscore dependency. Rename package for atmosphere publication.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-  name: "meteorblackbelt:underscore-deep",
+  name: "juto:underscore-deep",
   summary: "underscore-deep package",
-  version: "0.0.4",
-  git: "https://github.com/meteorblackbelt/meteor-underscore-deep",
+  version: "0.0.5",
+  git: "https://github.com/jutoapp/meteor-underscore-deep",
   documentation: null
 });
 
 
 Package.onUse(function (api) {
-  api.use(['coffeescript@1.0.6', 'underscore@1.0.3']);
+  api.use(['coffee:script@1.0.3', 'underscore@1.0.10']);
   api.imply('underscore');
   api.addFiles('underscore-deep.coffee');
   api.export('_d');
